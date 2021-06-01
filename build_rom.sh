b57d9a579b1bf333ceb96d641be55f8f1a8f7903
Rahif M <faizel326@gmail.com>
2021-06-02 04:10:52 +0530

    Trigger

--- build_rom.sh
+++ build_rom.sh
@@ -4,9 +4,9 @@ git clone https://github.com/RahifM/local_manifests --depth 1 -b staging/du-q10x
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8 || repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
-#source build/envsetup.sh
-#lunch du_mido-user
-#mka bacon
+source build/envsetup.sh
+lunch du_mido-user
+mka bacon
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
-#rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
+rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
