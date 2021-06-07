12b7ce92b45148a3ead8e5f540088ab09bf6c2c0
Rahif M <faizel326@gmail.com>
2021-06-07 23:22:49 +0530

    Add lunch [skip ci]

--- build_rom.sh
+++ build_rom.sh
@@ -5,6 +5,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 # build rom
 source build/envsetup.sh
+lunch pa_mido-userdebug
 export TZ=Asia/Kolkata
 ./rom-build.sh mido
 
