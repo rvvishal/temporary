6372b185090e61b25b9adbf66b6e520d7cbf561f
RahifM <faizel326@gmail.com>
2021-06-11 17:25:02 +0530

    Move to staging asb branch
    Signed-off-by: RahifM <faizel326@gmail.com>

--- build_rom.sh
+++ build_rom.sh
@@ -1,5 +1,5 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/AOSP-whatever/platform_manifest -b du-q10x -g default,-device,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u https://github.com/AOSP-whatever/platform_manifest -b staging/du-q10x_asb_2021-06 -g default,-device,-mips,-darwin,-notdefault
 git clone https://github.com/RahifM/local_manifests --depth 1 -b staging/du-q10x .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
