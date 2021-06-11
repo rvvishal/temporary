8a231c7ec9cb88a397b0715467aa5dd75d4e6fce
Flashokiller <75789819+flashokiller@users.noreply.github.com>
2021-06-11 16:17:29 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -3,7 +3,7 @@ repo init --depth=1 --no-repo-verify -u https://github.com/Spark-Rom/manifest -b
 git clone  --depth 1 https://github.com/flashokiller/mainfest_personal -b master .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
-# build rom
+# build rom:)
 . build/envsetup.sh
 lunch spark_ysl-userdebug
 export TZ=Asia/kolkata #put before last build command
