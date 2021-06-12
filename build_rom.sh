aae851906bb93ea41ab47670940a1344df6716ae
Flashokiller <75789819+flashokiller@users.noreply.github.com>
2021-06-12 22:13:31 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -3,7 +3,7 @@ repo init --depth=1 --no-repo-verify -u https://github.com/ArrowOS/android_manif
 git clone  --depth 1 https://github.com/flashokiller/mainfest_personal -b master .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
-# build rom:)..
+# build rom:)......
 . build/envsetup.sh
 lunch arrow_ysl-userdebug
 make bacon
