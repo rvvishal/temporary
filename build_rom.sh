9655bbd177dac21959d80284fe2cfffb46a3dfb1
Rahif M <faizel326@gmail.com>
2021-06-13 03:46:40 +0530

    Fix duplication?

--- build_rom.sh
+++ build_rom.sh
@@ -7,6 +7,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 source build/envsetup.sh
 lunch du_beryllium-eng
 #export WITH_GMS=true
+make installclean
 export TZ=Asia/Kolkata
 mka bacon
 
