afd0303d094a5725925e679506c8301b0c387fc5
Rahif M <faizel326@gmail.com>
2021-06-13 13:37:25 +0530

    Temp

--- build_rom.sh
+++ build_rom.sh
@@ -6,6 +6,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 # build rom
 source build/envsetup.sh
 lunch du_beryllium-eng
+export BUILD_BROKEN_DUP_RULES=true
 #export WITH_GMS=true
 export TZ=Asia/Kolkata
 mka bacon
