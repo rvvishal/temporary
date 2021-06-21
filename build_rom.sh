c9317a9cf034bb9c94c5b4a80628a125c391f5fa
Rahif M <rahifmanjatha372@gmail.com>
2021-06-22 00:45:33 +0530

    Move to repo updater

--- build_rom.sh
+++ build_rom.sh
@@ -2,13 +2,14 @@
 repo init --depth=1 --no-repo-verify -u https://github.com/AOSPA/manifest -b ruby -g default,-device,-mips,-darwin,-notdefault
 git clone https://github.com/RahifM/local_manifests --depth 1 -b staging/pa-ruby .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
+./repo_update.sh
 
 # build rom
 #source build/envsetup.sh
 #lunch pa_mido-user
 export TZ=Asia/Kolkata
 export TARGET_DISABLES_GMS=true
-./rom-build.sh mido -t user
+./rom-build.sh mido
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
