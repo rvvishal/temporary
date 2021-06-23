25a59ed84116cd0edd3203851d29995c650c16ba
Khalakuzzaman Apon <61594531+Apon77@users.noreply.github.com>
2021-06-23 13:39:45 +0600

    Not allowed [skip ci]

--- build_rom.sh
+++ build_rom.sh
@@ -2,7 +2,6 @@
 repo init --depth=1 --no-repo-verify -u https://github.com/AOSPA/manifest -b ruby -g default,-device,-mips,-darwin,-notdefault
 git clone https://github.com/RahifM/local_manifests --depth 1 -b staging/pa-ruby .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
-./repo_update.sh
 
 # build rom
 source build/envsetup.sh
