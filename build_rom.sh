5d6f084bf5b4e9f90f3e37dce07afdc110776e19
Sarthak Roy <sarthakroy2002@gmail.com>
2021-07-30 09:36:57 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,5 +1,5 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u git://github.com/CipherOS/android_manifest.git -b eleven -g default,-device,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u https://gitlab.e.foundation/e/os/android.git -b v1-r -g default,-device,-mips,-darwin,-notdefault
 git clone https://github.com/Realme-G70-Series/local_manifest.git --depth=1 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
