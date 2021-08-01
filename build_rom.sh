45a63a0f127ac0b46df48787a5038f6734f441e2
Sarthak Roy <sarthakroy2002@gmail.com>
2021-08-01 10:07:25 +0530

    INITIAL BUILD

--- build_rom.sh
+++ build_rom.sh
@@ -6,7 +6,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 # build rom
 source build/envsetup.sh
 lunch lineage_RMX2020-userdebug
-export TZ=Asia/Dhaka #put before last build command
+export TZ=Asia/Dhaka #put before last  build command
 mka bacon -j$(nproc --all) 
 
 # upload rom 
