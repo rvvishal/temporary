46de2ffd27a3eb3a9755b1328799910a36644032
Rahif M <rahifmanjatha372@gmail.com>
2021-08-04 18:25:07 +0530

    Trigger

--- build_rom.sh
+++ build_rom.sh
@@ -5,7 +5,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 # build rom
 source build/envsetup.sh
-#lunch lineageos_mido-userdebug
+#lunch lineage_mido-userdebug
 export TZ=Asia/Kolkata #put before last build command
 brunch mido
 
