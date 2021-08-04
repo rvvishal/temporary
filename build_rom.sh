4a1d5637844b4a893771a34c897b75a40d05e63e
Aditya Singh <adityasingh1768@gmail.com>
2021-08-04 20:38:43 +0530

    r2p: drop blobs

--- build_rom.sh
+++ build_rom.sh
@@ -5,7 +5,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 # build rom
 source build/envsetup.sh
-lunch awaken_r2p-userdebug
+lunch awaken_r2p-user
 export TZ=Asia/Dhaka #put before last build command
 m bacon
 
