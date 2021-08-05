b414c31978eb8cb1db5dc871ee4b5cc9b328d137
Mukul Sharma <87255315+Lucifer-morning-star-96@users.noreply.github.com>
2021-08-05 14:47:43 +0530

    Inite A10

--- build_rom.sh
+++ build_rom.sh
@@ -1,11 +1,11 @@
 # sync rom.  
-repo init --depth=1 --no-repo-verify -u https://github.com/Corvus-R/android_manifest.git -b 11 -g default,-device,-mips,-darwin,-notdefault
-git clone https://github.com/Lucifer-morning-star-96/frostmanifest.git --depth 1 -b Corvus .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u https://github.com/Corvus-Q/android_manifest.git -b 10 -g default,-device,-mips,-darwin,-notdefault
+git clone https://github.com/Lucifer-morning-star-96/frostmanifest.git --depth 1 -b Corvus-a10 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
                     
 # build rom 
 . build/envsetup.sh
-lunch corvus_lavender-user
+lunch du_lavender-userdebug
 export TZ=Asia/kolkata #put before last build command (Time)
 make corvus 
 
