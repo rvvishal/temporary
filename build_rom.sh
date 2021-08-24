4ba49b7f54ba78313964755d8235b6fcb9a11061
Rahul K <41164942+rk134@users.noreply.github.com>
2021-08-24 13:15:51 +0100

    add export line

--- build_rom.sh
+++ build_rom.sh
@@ -1,11 +1,12 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u https://github.com/xdroid-CAF/xd_manifest -b eleven -g default,-device,-mips,-darwin,-notdefault
-git clone https://github.com/rk134/local_manifests.git --depth 1 -b master .repo/local_manifests
+git clone https://github.com/rk134/local_manifests.git --depth 1 -b xdroid .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 . build/envsetup.sh
 lunch xdroid_vince-userdebug
+export SELINUX_IGNORE_NEVERALLOWS=true
 export TZ=Asia/Dhaka #put before last build command
 make xd
 
