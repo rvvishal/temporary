e0a6bfedffbb6032e1a60a5e695be7890eca7d3e
Flashokiller <75789819+flashokiller@users.noreply.github.com>
2021-08-25 09:11:52 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -3,11 +3,11 @@ repo init --depth=1 --no-repo-verify -u https://github.com/ProjectRadiant/manife
 git clone https://github.com/flashokiller/manifest_pine -b radinat --depth 1 -b master .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
-# build rom.
+# build rom.lmao
 source build/envsetup.sh
 lunch radiant_pine-userdebug
 export TZ=Asia/Kolkata #put before last build command
-mka bacon
+make bacon
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
