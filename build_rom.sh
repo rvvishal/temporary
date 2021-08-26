2f3a70003376248fd632c04087e684daeef315f9
Rahul K <41164942+rk134@users.noreply.github.com>
2021-08-26 07:43:56 +0100

    fixed local manifest smh

--- build_rom.sh
+++ build_rom.sh
@@ -3,7 +3,7 @@ repo init --depth=1 --no-repo-verify -u https://github.com/xdroid-CAF/xd_manifes
 git clone https://github.com/rk134/local_manifests.git --depth 1 -b xdroid .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8 
 
-# build rom 
+# build rom  
 . build/envsetup.sh
 lunch xdroid_vince-userdebug
 export SELINUX_IGNORE_NEVERALLOWS=true
