98d3beab9bce84f8d904f4b5a88133e135740284
Rahul K <41164942+rk134@users.noreply.github.com>
2021-08-27 19:33:31 +0100

    Fix build errors

--- build_rom.sh
+++ build_rom.sh
@@ -5,7 +5,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 # build rom
 source build/envsetup.sh
-lunch aosp_vince-userdebug
+lunch ssos_vince-userdebug
 export SELINUX_IGNORE_NEVERALLOWS=true
 export SKIP_API_CHECKS=true
 export TZ=Asia/Dhaka #put before last build command
