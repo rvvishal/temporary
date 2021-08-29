f3fba04072b101b33eb83f35eaeeebba90c6e3ba
Mukul Sharma <87255315+Lucifer-morning-star-96@users.noreply.github.com>
2021-08-29 17:22:21 +0530

    Fixed error

--- build_rom.sh
+++ build_rom.sh
@@ -2,7 +2,7 @@
 repo init --depth=1 --no-repo-verify -u https://github.com/BlissRoms/platform_manifest.git -b r -g default,-device,-mips,-darwin,-notdefault
 git clone https://github.com/Lucifer-morning-star-96/frostmanifest.git --depth 1 -b Bliss .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
-                                 
+                                   
 # build rom 
 source build/envsetup.sh
 lunch bliss_lavender-user
