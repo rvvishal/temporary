ab15cea5086f607999658e1d5962d4d34bc1fecc
AbrarNoob <70853665+AbrarNoob@users.noreply.github.com>
2021-09-10 18:01:22 +0600

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -6,7 +6,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 # build rom
 source build/envsetup.sh
-lunch awaken_vayu-userdebug
+lunch awaken_lavender-user
 export TZ=Asia/Dhaka #put before last build command
 mka bacon
 
