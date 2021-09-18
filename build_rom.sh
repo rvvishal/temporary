2bee17ba0c546946b31afb181945829719c5728a
HyperNotAryanX97 <ahirearyan2016@gmail.com>
2021-09-18 19:42:17 +0530

    begonia: Run Build

--- build_rom.sh
+++ build_rom.sh
@@ -1,12 +1,12 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u https://github.com/CherishOS/android_manifest.git -b eleven -g default,-device,-mips,-darwin,-notdefault
-git clone https://github.com/HyperNotAryanX97/Begonia.git --depth 1 -b backup .repo/local_manifests
+git clone https://github.com/HyperNotAryanX97/Begonia --depth 1 -b backup .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
 lunch cherish_begonia-userdebug
-export TZ=Asia/Kolkata #put before last build command
+export TZ=Asia/Dhaka #put before last build command
 mka bacon
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
